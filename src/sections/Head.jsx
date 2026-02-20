6b7d3afa4dfcd9168eaca17d3d601b2704323b2f
Rizwan Zaman <rizwan@example.com>
2026-02-21 02:07:21 +0500

    changes

--- src/sections/Head.jsx
+++ src/sections/Head.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Head = () => {
   return (
-    <section className='flex justify-center w-full h-[18] bg-[#D4C5A0] text-white text-2xl font-Roboto py-4 max-sm:text-lg max-sm:h-[15]'>
+    <section className='flex justify-center w-full h-[18] bg-[#D4C5A0] text-white text-2xl font-Roboto py-4 max-sm:text-lg max-sm:hidden'>
          GET 25% OFF ON RETURN RIDE!
     </section>
   )
